34a0a107b394518d4b76c4575f58b234e4c340de
chikaltyaga <tyaga.chikal@gmail.com>
2020-09-29 16:13:43 +0700

    Praktikum 2 A2.1900030

---
+++ A2.1900030/Praktikum 2/main.py
@@ -0,0 +1,19 @@
+class Hero: #template
+    pass
+
+hero1 = Hero() #object
+hero2 = Hero()
+hero3 = Hero()
+
+hero1.name = "sniper"
+hero.health = 100
+
+hero2.name = "medusa"
+hero.health = 500
+
+hero3.name = "sven"
+hero.health = 1000
+
+print(hero1)
+print(hero1.__dict__)
+print(hero1.name)
