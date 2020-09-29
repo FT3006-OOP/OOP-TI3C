9fe6e64362623bd09f852ce26ca28fc57bf4917c
A2.1810004 <a2.1810004@mhs.stmik-sumedang.ac.id>
2020-09-29 13:43:46 +0700

    Praktikum 2 Python

---
+++ A2.1810004/Praktikum_2/Main.py
@@ -0,0 +1,19 @@
+class Hero:
+    pass
+
+hero1 = Hero()
+hero2 = Hero()
+hero3 = Hero();
+
+hero1.name = "Fadhliatul"
+hero1.health = 1000
+
+hero2.name = "Widiya"
+hero2.health = 500
+
+hero3.name = "Akbar"
+hero3.health = 200
+
+print(hero1)
+print(hero1.__dict__)
+print(hero1.name)
