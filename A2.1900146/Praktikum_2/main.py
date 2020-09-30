5764f2a991818c03dfa5a57a7c26d11b6790aa5f
rekiramdani12 <leonel.rekyy26@gmail.com>
2020-09-30 13:34:32 +0700

    praktikum 2

---
+++ A2.1900146/Praktikum_2/main.py
@@ -0,0 +1,19 @@
+class hero: #template
+    pass
+
+hero1 = hero() #0bject
+hero2 = hero()
+hero3 = hero()
+
+hero1.name = "sniper"
+hero1.health = 100
+
+hero2.name = "sven"
+hero2.health = 200
+
+hero3.name = "ucup"
+her03.health = 1000
+
+print(hero1)
+print(hero1.__dict__)
+print(hero1.name)
