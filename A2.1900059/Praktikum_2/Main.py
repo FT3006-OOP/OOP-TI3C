20ac15862bdee39dd54518c77d29fd5772df60ab
muhammadnandijunaedi <mnandijunaedi@gmail.com>
2020-09-30 22:30:46 +0700

    membuat directori A2.1900059

---
+++ A2.1900059/Praktikum_2/Main.py
@@ -0,0 +1,19 @@
+class Hero: #template
+    pass
+
+hero1 = Hero() #object
+hero2 = Hero()
+hero3 = Hero();
+
+hero1.name ="Nandi"
+hero1.health = 1000
+
+hero2.name ="juned"
+hero2.health = 2000
+
+hero3.name ="Boy"
+hero3.health = 3000
+
+print(hero1)
+print(hero1.__dict__)
+print(hero1.name)
