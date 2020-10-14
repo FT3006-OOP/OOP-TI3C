59c122171cfa90c5d218c9c2b192c23ba0d1d3eb
muhammadnandijunaedi <mnandijunaedi@gmail.com>
2020-10-14 22:31:52 +0700

    Praktikum_4

--- A2.1900059/Praktikum_02/Main.py
+++ A2.1900059/Praktikum_02/Main.py
@@ -5,13 +5,13 @@ hero1 = Hero() #object
 hero2 = Hero()
 hero3 = Hero();
 
-hero1.name ="Nandi"
+hero1.name = "Nandi"
 hero1.health = 1000
 
-hero2.name ="juned"
+hero2.name = "Juned"
 hero2.health = 2000
 
-hero3.name ="Boy"
+hero3.name = "Boy"
 hero3.health = 3000
 
 print(hero1)
