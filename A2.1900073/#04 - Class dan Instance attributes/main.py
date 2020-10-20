e941bc3138afba3311d91a4213d4e2bd011b1281
gilangfajarramadhan <a2.1900073@mhs.stmik-sumedang.ac.id>
2020-10-20 19:59:59 +0700

    praktikum5

--- A2.1900073/#04 - Class dan Instance attributes/main.py
+++ A2.1900073/#04 - Class dan Instance attributes/main.py
@@ -2,7 +2,7 @@ class Hero: #template
     #class variable
     jumlah = 0
 
-    def __init__(self,inputname,inputHealth,inputPower,inputArmor):
+    def __init__(self,inputName,inputHealth,inputPower,inputArmor):
         # instance variable
         self.name = inputName
         self.health = inputHealth
@@ -16,5 +16,5 @@ hero1 = Hero("sniper",100, 10, 4)
 print(Hero.jumlah)
 hero2 = Hero("mirana",100, 15, 1)
 print(Hero.jumlah)
-hero3 = Hero("ucup".1000, 100, 0)
+hero3 = Hero("ucup",1000, 100, 0)
 print(Hero.jumlah)
