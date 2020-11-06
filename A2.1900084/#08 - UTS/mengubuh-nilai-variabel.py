c7dfd75616290b30cb9ecf9041980bda9cb179d5
Indrawan201130 <indrawansurya21@gmail.com>
2020-11-06 11:48:51 +0700

    UTS

---
+++ A2.1900084/#08 - UTS/mengubuh-nilai-variabel.py
@@ -0,0 +1,13 @@
+# angka_saya telah kita beri nilai 1945
+
+angka_saya = 1945
+
+# setelah didefenisikan dan diberi nilai
+# kita bisa nengubah nilainya
+
+angka_saya = 1
+
+# Untuk melihat kalau kita telah mengubahnya,
+# kita akan mencetaknya dengan print
+
+print(angka_saya)
