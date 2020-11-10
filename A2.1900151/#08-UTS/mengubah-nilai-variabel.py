730941c8170e88de7cef5f212aa79b12eb1658fb
rifkyiqbal20 <rifkyiqbalpermadi20@gmail.com>
2020-11-10 17:06:20 +0700

    UTS

---
+++ A2.1900151/#08-UTS/mengubah-nilai-variabel.py
@@ -0,0 +1,8 @@
+# kita bisa mengubah nilainya
+
+angka_saya = 1
+
+# Untuk melihat kalau kita telah mengubah nilainya,
+# kita akan mencetaknya dengan print
+
+print(angka_saya)
---
+++ A2.1900151/#08-UTS/pentingnya-spasi.py
@@ -0,0 +1,4 @@
+komputer = 1
+laptop = 2
+
+print(komputer, laptop)
