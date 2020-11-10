227dc18ded44da04b7f4fc8a5bb490eae986fac6
ratnakomala <ratnakomalla1@gmail.com>
2020-11-10 15:00:37 +0700

    UTS

---
+++ A2.1900143/#08 - UTS/Variabel_Valid.py
@@ -0,0 +1,8 @@
+P3MBU4T  = "Guido Van Rossum"  # nama variabel yang 'alay', tapi apakah valid
+
+# Variabel panjang nih
+contoh_perusahaan_yang_menggunakan_bahasa_python = "youtube, path, dan lain-lain"
+
+print(P3MBU4T)
+print(contoh_perusahaan_yang_menggunakan_bahasa_python)
+
