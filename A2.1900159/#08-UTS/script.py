a15c62121b3e319a00cc98c32d1497be619bc1f3
RoffiNurRohman <roffinurrohman22@gmail>
2020-11-11 14:26:52 +0700

    UTS

---
+++ A2.1900159/#08-UTS/script.py
@@ -0,0 +1,11 @@
+# angka_saya telah kita beri nilai 1945
+
+angka_saya = 1945
+
+# setelah didefinisikan dan diberi nilai
+# kita bisa mengubah nilainya
+
+angka_saya = 1
+
+# untuk melihat kalau kita telah mengubahnya,
+# kita akan mencetaknya dengan print
