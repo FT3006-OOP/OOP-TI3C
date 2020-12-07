468f68d8889977b680c3f81ae794ca8ebec49aac
SonySolahudinAbdurachman <sonymoonraker@gmail.com>
2020-12-07 13:54:14 +0700

    praktikum 11

--- A2.1900171/#11 - getter & setter_encapsulasi_pythonic_way/Main.py
+++ A2.1900171/#11 - getter & setter_encapsulasi_pythonic_way/Main.py
@@ -1,13 +1,14 @@
 class Hero:
 
     def __init__(self, name, health, armor):
-        self.name = name 
-        self.__health = health 
-        self.__armor = armor 
-        #self.info = "name {} : \n\thealth: {}".format(self.name,self.__health)
+        self.name = name
+        self.__health = health
+        self.__armor = armor
+
+#self.info = "name {} : \n\thealth: {}".format(self.name,self.__health)
 
     @property
-    def info(self):
+    def info (self):
         return "name {} : \n\thealth: {}".format(self.name,self.__health)
 
     @property
@@ -20,23 +21,18 @@ class Hero:
 
     @armor.setter
     def armor(self, input):
-        self.__armor = input
-
-    @armor.deleter
-    def armor(self):
-        print('armor di delet')
-        self.__armor = None 
+        self.__armor = None
 
 sniper = Hero('sniper',100,10)
 
-print('mengubah info')
+print('merubah info')
 print(sniper.info)
 sniper.name = 'dadang'
 print(sniper.info)
 
 print('getter dan setter untuk __armor:')
 print(sniper.armor)
-sniper.armor = 50
+sniper.armor = '50'
 print(sniper.armor)
 
 print('delete armor')
