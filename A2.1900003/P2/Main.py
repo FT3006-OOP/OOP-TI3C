daa81ccddd33329373b81f8f05d243d57e3e9f4e
arcviell <heryfirmansyah390@gmail.com>
2021-01-05 14:10:08 +0700

    UAS

---
+++ A2.1900003/P2/Main.py
@@ -0,0 +1,20 @@
+class Hero: #template
+    pass
+
+
+hero1 = Hero() # object / instance (instansiate)
+hero2 = Hero()
+hero3 = Hero();
+
+hero1.name = "acep"
+hero1.health = 2000
+
+hero2.name = "dante"
+hero2.health = 2000
+
+hero3.name = "vergil"
+hero3.health = 1000
+
+print(hero1)
+print(hero1.__dict__)
+print(hero1.name)
